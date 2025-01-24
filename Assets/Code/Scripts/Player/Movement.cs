8b6977d43b84e22d49402921341fbc0d60a57dd2
RedG <dibeneluigi@gmail.com>
2025-01-24 14:25:08 +0100

    Organization

--- Assets/Code/Scripts/Player/Movement.cs
+++ Assets/Code/Scripts/Player/Movement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Movement : MonoBehaviour
     
@@ -8,34 +9,39 @@ public class Movement : MonoBehaviour
     public float turnSpeed = 100f;         // Speed at which the player turns
     public float gravity = -9.8f;          // Gravity applied to the player
 
-    private CharacterController characterController;
+    private Rigidbody _rigidbody;
     private Vector3 velocity;              // To handle movement velocity
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        characterController = GetComponent<CharacterController>();  // Get the CharacterController component attached to the player
+        _rigidbody = GetComponent<Rigidbody>(); // Get the Rigidbody component attached to the player
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Get input from the player (WASD keys or arrow keys)
-        float moveDirectionZ = Input.GetAxis("Vertical");
         float moveDirectionX = Input.GetAxis("Horizontal");
 
+        // Handle player rotation (rotate towards the movement direction)
+        if (moveDirectionX != 0)
+        {
+            Vector3 toRotation = transform.rotation.eulerAngles;  // Create a rotation to face the direction of movement
+            toRotation.y += moveDirectionX * turnSpeed;
+            transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler (toRotation), turnSpeed * Time.deltaTime);  // Rotate smoothly
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Get input from the player (WASD)
+        float moveDirectionZ = Input.GetAxis("Vertical");
+
         // Create a movement vector based on input
-        Vector3 move = transform.right * moveDirectionX + transform.forward * moveDirectionZ;
+        Vector3 move = /*transform.right * moveDirectionX + */transform.forward * moveDirectionZ;
 
         // Apply movement
-        characterController.Move(move * speed * Time.deltaTime);
-
-        // Handle player rotation (rotate towards the movement direction)
-        if (moveDirectionZ != 0 || moveDirectionX != 0)
-        {
-            Quaternion toRotation = Quaternion.LookRotation(move, Vector3.up);  // Create a rotation to face the direction of movement
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, turnSpeed * Time.deltaTime);  // Rotate smoothly
-        }
+        _rigidbody.AddForce(move * speed);
     }
 }
 
