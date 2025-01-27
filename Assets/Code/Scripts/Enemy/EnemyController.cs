28f95918335fec0e32cba0e2d3026a8929f8a1d8
RedG <dibeneluigi@gmail.com>
2025-01-27 15:55:20 +0100

    Enemy Sistem

---
+++ Assets/Code/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyController : MonoBehaviour
+{
+    [field: SerializeField, Header("Patrol Settings")]
+    public Transform[] PatrolPoints { get; private set; }
+    [field: SerializeField] public PatrolState PatrolState { get; private set; }
+
+    [field: SerializeField, Header("Chase Settings")]
+    public Transform PlayerTransform { get; private set; }
+    [field: SerializeField] public ChaseState ChaseState { get; private set; }
+
+    public NavMeshAgent Agent { get; private set; }
+
+    private EnemyState _currentState;
+
+    private void Awake()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+
+        SetState(PatrolState);
+    }
+
+    private void Update()
+    {
+        if (_currentState != null) _currentState.OnUpdate(this);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_currentState != null) _currentState.OnCollision(this, other);
+    }
+
+    public void SetState(EnemyState _state)
+    {
+        if (_state == null) return;
+
+        if (_currentState != null)
+            _currentState.OnExit(this);
+
+        _currentState = _state;
+
+        _currentState.OnEnter(this);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_currentState != null) _currentState.DrawGizmo(this);
+    }
+}
---
+++ Assets/Code/Scripts/Enemy/EnemyPatrol.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [SerializeField] private float playerSightRadius = 20f;
+    [SerializeField] private float flagSightRadius = 5f;
+
+    private int _currentIndex = 0;
+
+    public override void OnEnter(EnemyController _controller)
+    {
+        float closestDistance = float.MaxValue;
+
+        for (var index = 0; index < _controller.PatrolPoints.Length; index++)
+        {
+            var patrolPoint = _controller.PatrolPoints[index];
+
+            float distance = Vector3.Distance(_controller.transform.position, patrolPoint.position);
+
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                _currentIndex = index;
+            }
+        }
+
+        _controller.Agent.SetDestination(_controller.PatrolPoints[_currentIndex].position);
+    }
+
+    public override void OnUpdate(EnemyController _controller)
+    {
+        if (Vector3.Distance(_controller.transform.position, _controller.Agent.destination) < 0.1f)
+        {
+            _currentIndex += 1;
+
+            if (_currentIndex >= _controller.PatrolPoints.Length)
+            {
+                _currentIndex = 0;
+            }
+
+            _controller.Agent.SetDestination(_controller.PatrolPoints[_currentIndex].position);
+        }
+
+        if (Vector3.Distance(_controller.transform.position, _controller.PlayerTransform.position) < playerSightRadius)
+        {
+            _controller.SetState(_controller.ChaseState);
+        }
+    }
+
+    public override void OnExit(EnemyController _controller)
+    {
+    }
+
+    public override void OnCollision(EnemyController _controller, Collider _collision)
+    {
+    }
+
+    public override void DrawGizmo(EnemyController _controller)
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_controller.transform.position, playerSightRadius);
+
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(_controller.transform.position, flagSightRadius);
+    }
+}
