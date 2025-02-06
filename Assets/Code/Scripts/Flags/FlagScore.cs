ec3afac68c8110cc2a155603ec857e3fd9cb1a6d
RedG <dibeneluigi@gmail.com>
2025-02-06 11:28:58 +0100

    Commit sound

--- Assets/Code/Scripts/Flags/FlagScore.cs
+++ Assets/Code/Scripts/Flags/FlagScore.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class FlagScore : MonoBehaviour
 {
     [SerializeField] private int amount = 1;
+    public AudioSource audioSource;
+    void Start()
+    {
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
 
     //se le bandiere collidono con qualcos'altro, aumenta il punteggio e si disattivano
     private void OnTriggerEnter(Collider other)
@@ -11,6 +20,7 @@ public class FlagScore : MonoBehaviour
         if (other.TryGetComponent(out ScoreManager manager))
         {
             manager.AddScore(amount);
+            audioSource.Play();
             gameObject.SetActive(false);
         }
     }
--- Assets/Code/Scripts/Flags/FlagScore_E.cs
+++ Assets/Code/Scripts/Flags/FlagScore_E.cs
@@ -4,7 +4,7 @@ public class FlagScore_E : MonoBehaviour
 {
     [SerializeField] private int amount = 1;
 
-    //se le bandiere collidono con qualcos'altro, aumenta il punteggio e si disattivano
+    //if flags collied with something, add score and disable
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy"))
